b3ad6f42c6758d589e6d67cc2b04c573c4f35874
Pinelli <ricardosilvapinelli@gmail.com>
2023-05-16 14:13:00 -0300

    Finish

--- app.js
+++ app.js
@@ -5,43 +5,54 @@ const buttonValidate = document.getElementById('validate-button');
 
 const userCPF = () => inputCPF.value;
 
-/*
-  dígito: 0 * 10
-  dígito: 1 * 9
-  dígito: 2 * 8
-  dígito: 3 * 7
-  dígito: 4 * 6
-  dígito: 5 * 5
-  dígito: 6 * 4
-  dígito: 7 * 3
-  dígito: 8 * 2
-
-  for (let j = 10; j >= 2; j--) {
-    console.log(`Digito: ${ArrCPF[i]} * ${j}`);
-  }
-*/
-
-const validateCPF = () => {
-  let ArrCPF = userCPF().split('');
+const validateCPF = (cpf) => {
+  let ArrCPF = cpf.split('');
   let validator = 10;
+  let validator2 = 11;
   let numbersToValidate = [];
-  let totalNumbersValidate;
-  let verifyingDigit;
-
+  let numbersToValidate2 = [];
+  let totalNumbersValidate = 0;
+  let totalNumbersValidate2 = 0;
+  let verifyingDigit = 0;
+  let verifyingDigit2 = 0;
+  
+  // first validate
   for (let i = 0; i < 9; i++) {
-    numbersToValidate.push(ArrCPF[i] * validator);
+    numbersToValidate.push((ArrCPF[i] * validator));
     validator--;
   };
   numbersToValidate.map(item => totalNumbersValidate += item);
   let restValidate = totalNumbersValidate % 11;
 
-  // if(restValidate === 0 || 1) verifyingDigit = 0;
-  // else verifyingDigit = Number(11 - restValidate);
+  if(restValidate === 0 || restValidate === 1) verifyingDigit = 0;
+  else verifyingDigit = 11 - restValidate;
+
+  if(verifyingDigit != cpf[9]) {
+    console.log('Primeira verificação')
+    return false;
+  }
   
-  console.log(`${userCPF()[9]} | ${restValidate} | ${verifyingDigit}`);
+  // second validate
+  for (let i = 0; i < 11; i++) {
+    numbersToValidate2.push((ArrCPF[i] * validator2));
+    validator2--;
+  };
+  numbersToValidate2.map(item => totalNumbersValidate2 += item);
+  let restValidate2 = totalNumbersValidate2 % 11;
+
+  if(restValidate2 === 0 || restValidate2 === 1) verifyingDigit2 = 0;
+  else verifyingDigit2 = 11 - restValidate2;
+  
+  if(verifyingDigit2 != cpf[10]) {
+    console.log('Segunda verificação')
+    return false;
+  };
+
+  return true;
 };
 
 const handleClick = () => {
-  validateCPF();
+  validateCPF(userCPF());
+  console.log(validateCPF(userCPF()))
 };
 buttonValidate.addEventListener('click', handleClick);
